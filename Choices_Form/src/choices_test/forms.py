ad50506dd780bdae852d897a79aa8d17d01d4354
elBatel <belhadjatech@gmail.com>
2019-10-18 07:48:21 +0100

    delete forms.py

--- Choices_Form/src/choices_test/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserInfo
-
-class UserInfoForm(forms.ModelForm):
-    class Meta:
-        model   = UserInfo
-        fields  = ['first_name', 'last_name','user_type']
